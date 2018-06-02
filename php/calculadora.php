cb78112437fbba9ca7767c19bfeabfa045879f6d
IRENE-PC\Irene <irenefrutosgonzalez@gmail.com>
2018-06-02 18:03:28 +0200

    cambios diseño calculadora

--- php/calculadora.php
+++ php/calculadora.php
@@ -5,7 +5,9 @@ $agetext = $_POST['ageText'];
 $weigth = $_POST['weigthValue'];
 $activity = $_POST['actValue'];
 $percent;
-echo "pet: ".$pet."<br>peso: ".$weigth." kilos"."<br>edad: ".$agetext."<br>actividad: ".$activity."<br><br>Porcentaje recomendado: ";
+/*echo "pet: ".$pet."<br>peso: ".$weigth." kilos"."<br>edad: ".$agetext."<br>actividad: ".$activity."<br><br>Porcentaje recomendado: ";
+*/
+echo "Porcentaje recomendado: ";
 
 switch($pet){
     case "dog":
