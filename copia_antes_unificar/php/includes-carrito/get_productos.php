6dc4694ced4a8d78a57c6bbce309965ba137c67c
OWLINPC\luisg <esperanzareinaponce@gmail.com>
2018-06-11 20:47:32 +0200

    copia antigua

---
+++ copia_antes_unificar/php/includes-carrito/get_productos.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Esto le dice a PHP que usaremos cadenas UTF-8 hasta el final
+mb_internal_encoding('UTF-8'); 
+// Esto le dice a PHP que generaremos cadenas UTF-8
+mb_http_output('UTF-8');
+
+require_once('conexion.php');
+header('Content-Type: text/json');
+header("Cache-Control: no-cache, must-revalidate");
+
+$myArray = array();
+$consulta= 'SELECT * FROM productos';
+if ($resultado = $conn->query($consulta)) {
+
+    while($row = $resultado->fetch_assoc()) {
+            $myArray[] = $row;
+    }
+    echo json_encode($myArray);
+}
+
+$resultado->close();
+$conn->close();
