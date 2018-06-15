5c786868cb8454ca493bf8cc5ad807a05c84efaf
OWLINPC\luisg <esperanzareinaponce@gmail.com>
2018-06-15 17:51:40 +0200

    cambios

--- proyecto_unificado/includes/productos/get_productos.php
+++ proyecto_unificado/includes/productos/get_productos.php
@@ -13,9 +13,8 @@ require_once('../conexion.php');
 header('Content-Type: text/json');
 header("Cache-Control: no-cache, must-revalidate");
 $myObj = new stdClass();
-$myObj->loggedin = isset($_SESSION['loggedin']);
 $myObj->list = array();
-if($myObj->loggedin && isset($_GET['typeId'])) {
+if(isset($_GET['typeId'])) {
     $typeId = $_GET['typeId'];
     $consulta= 'SELECT * FROM productos WHERE type_id = '.$typeId;
     if ($resultado = $conn->query($consulta)) {
