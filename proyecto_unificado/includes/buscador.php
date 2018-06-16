f83874babb13836fd2d5a715417ab6bd94567b8d
OWLINPC\luisg <esperanzareinaponce@gmail.com>
2018-06-16 20:16:21 +0200

    no message

--- proyecto_unificado/includes/buscador.php
+++ proyecto_unificado/includes/buscador.php
@@ -1,88 +1,37 @@
 <?php
-/*
-// AUTHOR: webreunidos.es
+session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// Primero definimos la conexión a la base de datos
-define('HOST_DB', 'localhost');  //Nombre del host, nomalmente localhost
-define('USER_DB', 'root');       //Usuario de la bbdd
-define('PASS_DB', '');           //Contraseña de la bbdd
-define('NAME_DB', 'tienda'); //Nombre de la bbdd
+// Esto le dice a PHP que usaremos cadenas UTF-8 hasta el final
+mb_internal_encoding('UTF-8'); 
+// Esto le dice a PHP que generaremos cadenas UTF-8
+mb_http_output('UTF-8');
 
-// Definimos la conexión
-function conectar(){
-	global $conexion;  //Definición global para poder utilizar en todo el contexto
-	$conexion = mysqli_connect(HOST_DB, USER_DB, PASS_DB, NAME_DB)
-	or die ('NO SE HA PODIDO CONECTAR AL MOTOR DE LA BASE DE DATOS')
-	or die ('NO SE ENCUENTRA LA BASE DE DATOS ' . NAME_DB);
-}
-*/
 require_once('conexion.php');
-/*
-function desconectar(){
-	global $conexion;
-	mysqli_close($conexion);
-}
-*/
-//Variable que contendrá el resultado de la búsqueda
-$texto = '';
-//Variable que contendrá el número de resgistros encontrados
-$registros = '';
+header('Content-Type: text/json');
+header("Cache-Control: no-cache, must-revalidate");
 
-if($_POST){
-    //trim — Elimina espacio en blanco (u otro tipo de caracteres) del inicio y el final de la cadena
-    $busqueda = trim($_POST['buscar']);
 
-    $entero = 0;
-    if (empty($busqueda)){
-        $texto = 'Búsqueda sin resultados';
-    }else{
-        // Si hay información para buscar, abrimos la conexión
-      //  conectar();
-        //mysql_set_charset('utf8');  // mostramos la información en utf-8
-        
-        //Contulta para la base de datos, se utiliza un comparador LIKE para acceder a tod lo que contenga la cadena a buscar
-        $sql = "SELECT * FROM productos WHERE nombre LIKE '%" .$busqueda. "%' ORDER BY nombre";
-        
-        $resultado = $conn->query($sql); //Ejecución de la consulta
-        //Si hay resultados...
-        if (mysqli_num_rows($resultado) > 0){
-            // Se recoge el número de resultados
-            $registros = '<p>HEMOS ENCONTRADO ' . mysqli_num_rows($resultado) . ' registros </p>';
-            // Se almacenan las cadenas de resultado
-            while($fila = mysqli_fetch_assoc($resultado)){
-                $texto .= $fila['nombre']. " " .$fila['descripcionCorta']. " " .$fila['precio']. " €" . '<br />';
+$myObj = new stdClass();
+$myObj->list = array();
+$myObj->loggedin = isset($_SESSION['loggedin']);
+if(isset($_GET['typeId'])) {//si se pone $myObj->loggedin cuando no esta logeado no salen los articulos
+    $typeId = $_GET['typeId'];
+    if($_GET){
+        $busqueda = trim($_GET['buscar']);
+        if (!empty($busqueda)){
+            $sql = "SELECT * FROM productos WHERE nombre LIKE '%$busqueda%' AND type_id = $typeId ORDER BY nombre";
+            if ($resultado = $conn->query($sql)){
+                // $registros = '<p>HEMOS ENCONTRADO ' . mysqli_num_rows($resultado) . ' registros </p>';
+                while($row = $resultado->fetch_assoc()) {
+                    $myObj->list[] = $row;
                 }
-        
-        }else{
-                $texto = "NO HAY RESULTADOS EN LA BBDD";	
+            }
         }
-        // Cerramos la conexión (por seguridad, no dejar conexiones abiertas)
-       // mysqli_close($conexion);
-    }
+    }    
 }
+echo json_encode($myObj);
+$conn->close();
 ?>
-
-<!--Ahora toca definir un formulario donde poder realizar nuestras búsquedas o consultas.-->
-<!-- 
-<!DOCTYPE html>
-<html lang="es-ES">
-<head> 
-<meta charset='utf-8'>
-<head> 
-<body>
-<h1>Ejemplo de buscado</h1> 
-<form id="buscador" name="buscador" method="post" action="<?php //echo $_SERVER['PHP_SELF'] ?>"> 
-    <input id="buscar" name="buscar" type="search" placeholder="Buscar aquí..." autofocus >
-    <input type="submit" name="buscador" class="boton peque aceptar" value="buscar">
-</form>
--->
-<?php 
-// Resultado, número de registros y contenido.
-echo $registros;
-echo $texto; 
-?>
-</body>
-</html>
-
-<!--https://webreunidos.es Más tutoriales en nuestra web-->
-<!--https://www.webreunidos.es/blog/crear-buscador-php-web-sencillo/-->
---
+++ proyecto_unificado/js/script-buscador.js
@@ -0,0 +1,86 @@
+function getXMLRequest() {
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        return new XMLHttpRequest();
+    } else {  // code for IE6, IE5
+        return new ActiveXObject('Microsoft.XMLHTTP');
+    }
+}
+
+function search(typeId) {
+    var value = $('#buscar').val().trim();
+    if(value == ""){
+        alert("Introduce algun valor en el buscador");
+    } else {
+        var xhttp = getXMLRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var data = JSON.parse(this.response);
+                var html = '',
+                productos = data.list;
+                for (var i = 0, iLen = productos.length; i < iLen; i++) {
+                    //alert(productos[i].image);
+                    html +=
+                        "  <div class=\"col-md-3\" id=\"caja\">\n" +
+                        "    <div class=\"thumbnail\" >\n" +
+                        "      <img src='"+productos[i].image+"' width='120'>" +
+                        "      <div class=\"caption\">\n" +
+                        "        <h3 class='titulo' id='prod_\"+productos[i].id+\"'>" + productos[i].nombre + "</h3>"+"<p class='descripcion'>"+productos[i].descripcionCorta+"</p>\n";
+                    if(productos[i].unidades<1){
+                        html += "<i class='fas fa-battery-empty unidades' title='Producto agotado' style='color: red'></i>";
+    
+                    }else if(productos[i].unidades >= 1 && productos[i].unidades <50 ){
+                        html += "<i class='fas fa-battery-half unidades' title='Quedan pocas unidades' style='color: orange'></i>";
+    
+                    }else{
+                        html += "<i class='fas fa-battery-full unidad unidades' title='Producto disponible' style='color: green'></i>";
+                    }
+    
+    
+                        html+= "          <br><label class='precio'>"+ productos[i].precio+"€</label>"+
+                        "         <p>\n";
+    
+                    if (data.loggedin == true) {
+                        if(productos[i].unidades>=1){
+                            html += "         <input type='button' class='btn btnn' onClick='anadirProducto(" + productos[i].id + ")' value='Añadir'/>"+
+    
+                                "         <input type='button'class='btn btnn' value='Ver detalle'/>";
+                        }else{
+                            html += "         <input type='button' class='btn btnn' onClick='anadirProducto(" + productos[i].id + ")' disabled value='Añadir'/>"+
+    
+                                "         <input type='button'class='btn btnn' value='Ver detalle'/>";
+                        }
+    
+                    }
+                    html += "        </p>\n" +
+                        "      </div>\n" +
+                        "    </div>\n" +
+                        "  </div>\n";
+                }
+                if(productos.length == 0) {
+                    html = '<div>No hay resultados</div>';
+                }
+                document.getElementById('productosContenedor').innerHTML = html;
+            }
+        };
+        xhttp.open('GET', './includes/buscador.php?typeId=' + typeId + '&buscar='+value+'&sid=' + Math.random(), true);
+        xhttp.send();
+    }
+}
+
+function limpiar(typeId){
+    $('#buscar').val('')
+    renderProductos(typeId);  
+    /*
+    var xhttp = getXMLRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var buscador = document.getElementById("buscar");
+            buscador.innerHTML = "";
+            renderProductos(typeId);
+        }  
+        xmlhttp.open('GET', './includes/productos/get_productos.php?typeId=' + typeId + '&sid=' + Math.random(), true);
+        xhttp.send();
+    }
+    */
+}
